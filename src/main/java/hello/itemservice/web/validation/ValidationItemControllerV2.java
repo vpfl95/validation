2a6274fa549d8177ed2325ba79392aa65d567743
vpfl95 <k_dh610@naver.com>
2023-03-29 16:47:13 +0900

    Validator 분리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -26,6 +26,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private  final  ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -73,8 +74,7 @@ public class ValidationItemControllerV2 {
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
-            log.info("errors={}",errors);
-            model.addAttribute("errors",bindingResult);
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
 
@@ -88,8 +88,6 @@ public class ValidationItemControllerV2 {
     //@PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
 
-        //검증 오류 결과를 보관
-        Map<String,String> errors = new HashMap<>();
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(),false,null,null, "상품 이름은 필수입니다."));
@@ -111,8 +109,7 @@ public class ValidationItemControllerV2 {
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
-            log.info("errors={}",errors);
-            model.addAttribute("errors",bindingResult);
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
 
@@ -126,8 +123,6 @@ public class ValidationItemControllerV2 {
     //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
 
-        //검증 오류 결과를 보관
-        Map<String,String> errors = new HashMap<>();
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(),false, new String[]{"required.item.itemName"},null,null));
@@ -149,8 +144,7 @@ public class ValidationItemControllerV2 {
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
-            log.info("errors={}",errors);
-            model.addAttribute("errors",bindingResult);
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
 
@@ -161,14 +155,12 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
 
         log.info("objectName={}",bindingResult.getObjectName());
         log.info("target={}",bindingResult.getTarget());
 
-        //검증 오류 결과를 보관
-        Map<String,String> errors = new HashMap<>();
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){
             bindingResult.rejectValue("itemName", "required");
@@ -190,8 +182,25 @@ public class ValidationItemControllerV2 {
 
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()){
-            log.info("errors={}",errors);
-            model.addAttribute("errors",bindingResult);
+            log.info("errors={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes , Model model) {
+
+        itemValidator.validate(item,bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
             return "validation/v2/addForm";
         }
 
